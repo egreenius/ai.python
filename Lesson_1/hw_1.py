9037c930a091645967a2604b9a3bde685a6d02f7
Eugene Pashkevich <ep@ftclub.biz>
2020-11-03 19:08:56 +0300

    - second Exercise complete

--- Lesson_1/hw_1.py
+++ Lesson_1/hw_1.py
@@ -22,8 +22,32 @@ while True: # Организуем бесконечный цикл для пол
     except ValueError:
         print('Вы ввели строку: ', var_number)
         continue
-
+print('Завершение задачи №1')
 '''
 Exercise 2
 '''
+# Пользователь вводит число, мы интерпретируем число как секунды, и выводим в формате "чч:мм:сс"
+while True: # Организуем бесконечный цикл для пользовательского ввода
+    var_number = input('Введите целое количество секунд: ')
+    try:  # вставляем перехват ошибок
+        var_number = int(var_number) # если данное преобразование закончится ошибкой, значит пользователь ввел не целое число
+        if var_number < 0:
+            print('Вы ввели отрицательное число: ', var_number)
+            continue
+        print('Вы ввели количество секунд равное: ', var_number)
+        break
+    except ValueError:
+        if not var_number:
+            print('Вы ввели пустую строку: <пусто>')
+        continue
+
+print('Вы ввели строку: ', var_number)
+hour = var_number // 3600
+minute = var_number % 3600//60
+second = var_number % 60
+time = f"{hour:02}:{minute:02}:{second:02}"
+print("Ваше время: ", time)
+
+print('Завершение задачи №2')
+
 print('Завершение программы')
